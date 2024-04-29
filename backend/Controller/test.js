09533d97bf3b51e3c0f68c5f7ded935df2b7de8d
kazi <kazi.kamil13@gmail.com>
2024-04-29 21:08:22 +0100

    m

--- backend/Controller/test.js
+++ backend/Controller/test.js
@@ -3,11 +3,6 @@ const router = express.Router();
 const EntreeController = require('../Controller/EntreeController');
 const authMiddleware = require('../Middlewares/authMiddleware');
 const multer = require('../Middlewares/multer');
-// router.get(
-//   '/ficheBesoins',
-//   authMiddleware,
-//   EntreeController.genererFicheBesoins
-// );
 router.post(
   '/bonCommande',
   authMiddleware('bon commande'),
