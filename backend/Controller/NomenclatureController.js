ef6df350dfc5493e0cb3eb743905450ebb996c9b
Hadi <baghdadli.abdelhadi@gmail.com>
2024-06-04 21:10:38 +0100

    changes from backend

--- backend/Controller/NomenclatureController.js
+++ backend/Controller/NomenclatureController.js
@@ -172,7 +172,10 @@ function deleteFournisseur(req, res) {
             .json({ response: 'prohibited to delete fournisseur' });
         });
     })
-    .catch(() => res.status(403).json({ response: 'forbidden' }));
+    .catch(err => {
+      console.log(err);
+      res.status(403).json({ response: 'forbidden' });
+    });
 }
 function showFournisseurs(req, res) {
   NomenclatureModel.getFournisseurs()
@@ -234,7 +237,8 @@ function updateFournisseur(req, res) {
     .then(() => {
       res.status(200).json({ response: 'fournisseur updated' });
     })
-    .catch(() => {
+    .catch(err => {
+      console.log(err);
       res.status(500).json({ response: 'internal error' });
     });
 }
--- backend/Models/NomenclatureModel.js
+++ backend/Models/NomenclatureModel.js
@@ -102,7 +102,7 @@ function isUsedFournisseur(fournisseur) {
     const query = `select id_fournisseur from fournisseur where raison_sociale=?
     and id_fournisseur in
       (select id_fournisseur from bon_de_commande)`;
-    const values = [article];
+    const values = [fournisseur];
 
     connection.connect(err => {
       if (err) {
@@ -117,6 +117,7 @@ function isUsedFournisseur(fournisseur) {
           reject('request error');
           return;
         }
+        console.log({ length: results.length });
         if (results.length == 0) resolve('');
         else reject('prohibited');
       });
@@ -163,7 +164,7 @@ function updateFournisseur(
     if (telephone) values.push(telephone);
     if (fax) values.push(fax);
     if (numRegistre) values.push(numRegistre);
-    if (rib) values.push(ribRip);
+    if (ribRip) values.push(ribRip);
     if (nif) values.push(nif);
     if (nis) values.push(nis);
     values.push(raisonSociale);
