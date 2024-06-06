7c3658b7b2db0e0ce0c01069a95d757f466245e9
kazi <kazi.kamil13@gmail.com>
2024-06-06 10:37:59 +0100

    m

--- backend/Models/UserModel.js
+++ backend/Models/UserModel.js
@@ -309,10 +309,11 @@ function getUser(email)
 {
   return new Promise((resolve, reject) => {
     const connection = mysql.createConnection(connectionConfig);
-    const query=`SELECT u.email, u.nom, u.prenom, u.active, u.date_naissance, r.designation as role, s.designation as structure, u.type
+    const query=`SELECT u.email, u.nom, u.prenom, u.active, u.date_naissance, r.designation as role, s.designation as structure, u.type,t.designation as structure_resp
     FROM utilisateur u
     LEFT JOIN role r ON u.id_role = r.id_role
     LEFT JOIN structure s ON s.id_structure = u.id_structure
+    Left join structure t On u.email=s.id_resp
     WHERE u.email = ?;`
     const values=[email]
     connection.connect((err) => {
